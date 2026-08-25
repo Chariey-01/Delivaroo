edec5b311a0b0079f53e68ae9a1496d51f820742
Chariey-01 <charityjepkoech007@gmail.com>
2026-08-25 13:14:59 +0300

    feat: add initial database migration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from app.config import Config
 from app.extensions import jwt,migrate,db
+from app import models 
 
 def create_app(config_class=Config):
   # create and configure the flask application
