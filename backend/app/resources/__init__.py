c4950db037b43265c3d88c6c523648534b89250b
Dazed31 <darrenamore13@gmail.com>
2026-08-26 23:51:19 +0300

    feat: add parcel destination update endpoint

--- backend/app/resources/__init__.py
+++ backend/app/resources/__init__.py
@@ -1,11 +1,10 @@
 from flask_restful import Api
-
 from app.resources.auth import LoginResource, RegisterResource
+from app.resources.parcel import ParcelResource
 
 
 def init_resources(app):
     api = Api(app)
-
     api.add_resource(RegisterResource, "/api/auth/register")
     api.add_resource(LoginResource, "/api/auth/login")
-    
+    api.add_resource(ParcelResource, "/api/parcels/<uuid:parcel_id>")
