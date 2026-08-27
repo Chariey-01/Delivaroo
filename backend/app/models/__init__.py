e87cd43f3f4aeba8303441806f945ddec3c4be46
Chariey-01 <charityjepkoech007@gmail.com>
2026-08-27 11:50:45 +0300

    feat(audit): add audit log model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,5 +4,6 @@ from app.models.parcel import Parcel
 from app.models.refresh_token import RefreshToken
 from app.models.status_history import StatusHistory
 from app.models.password_reset_token import PasswordResetToken
+from app.models.audit_log import AuditLog
 
-__all__ = ["User", "WeightCategory", "Parcel","RefreshToken", "PasswordResetToken","StatusHistory]
+__all__ = ["User", "WeightCategory", "Parcel","RefreshToken", "PasswordResetToken","StatusHistory", "AuditLog"]
