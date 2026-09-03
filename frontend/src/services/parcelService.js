812010382357088edb6298712bd0f44cf8c2819f
bhokenatasha810@gmail.com <bhokenatasha810@gmail.com>
2026-09-03 10:27:33 +0300

    feat: add parcel API service and redux thunks

--- frontend/src/services/parcelService.js
+++ frontend/src/services/parcelService.js
@@ -1,14 +1,42 @@
-import {
-  cancelParcel,
-  createParcel,
-  getParcel,
-  listParcels,
-  updateDestination
-} from '../api/parcels';
+import axios from "axios";
 
-export { cancelParcel, createParcel, getParcel };
-export const getParcels = listParcels;
-export const updateParcel = (parcelId, parcelData) =>
-  updateDestination(parcelId, parcelData.destination ?? parcelData);
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
-export default { cancelParcel, createParcel, getParcel, getParcels, updateParcel };
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+// Attach token if present
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// Parcel API calls
+export const createParcel = async (parcelData) => {
+  const response = await api.post("/parcels", parcelData);
+  return response.data;
+};
+
+export const listParcels = async () => {
+  const response = await api.get("/parcels");
+  return response.data;
+};
+
+export const getParcelDetails = async (id) => {
+  const response = await api.get(`/parcels/${id}`);
+  return response.data;
+};
+
+export const updateParcel = async (id, parcelData) => {
+  const response = await api.put(`/parcels/${id}`, parcelData);
+  return response.data;
+};
+
+export const cancelParcel = async (id) => {
+  const response = await api.patch(`/parcels/${id}/cancel`);
+  return response.data;
+};
